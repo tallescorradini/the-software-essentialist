8cd262b6780fff85b0ca7160f3b36516a51180dc
Talles Corradini <tallescorradini@gmail.com>
2023-03-29 22:45:39 -0300

    [Refactor]: replace if statements

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
@@ -1,36 +1,21 @@
 export class PasswordValidator {
   public static validate(password: string) {
-    if (password.length < 5)
-      return {
-        result: false,
-        errors: ['Too short!'],
-      };
+    const validationRules = [
+      password.length < 5 ? 'Too short!' : null,
+      password.length > 15 ? 'Too long!' : null,
+      !/[0-9]+/.test(password) ? 'It must contain at least 1 digit' : null,
+      !/[A-Z]+/.test(password)
+        ? 'It must contain at least 1 upper case letter'
+        : null,
+    ];
 
-    if (password.length > 15)
-      return {
-        result: false,
-        errors: ['Too long!'],
-      };
+    const errors = validationRules.filter((errorMessage) => !!errorMessage);
 
-    if (!/[0-9]+/.test(password) && !/[A-Z]+/.test(password))
-      return {
-        result: false,
-        errors: [
-          'It must contain at least 1 digit',
-          'It must contain at least 1 upper case letter',
-        ],
-      };
+    const isValid = errors.length === 0;
 
-    if (!/[0-9]+/.test(password))
-      return {
-        result: false,
-        errors: ['It must contain at least 1 digit'],
-      };
-
-    if (!/[A-Z]+/.test(password))
-      return {
-        result: false,
-        errors: ['It must contain at least 1 upper case letter'],
-      };
+    return {
+      result: isValid,
+      errors: errors,
+    };
   }
 }
